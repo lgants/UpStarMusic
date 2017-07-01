4a5992659fd5102cabac4802532a88557c3eb82a
lgants <lgants@gmail.com>
2017-07-01 18:35:15 -0500

    created album and artist models

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,16 @@
 // Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: String,
+  image: String,
+  revenue: Number
+});
+
+// don't create a model because this is a subdocument of artist
+
+module.exports = AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,25 @@
 // Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AlbumSchema = require('./album');
+
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [{
+    type: Schema.Types.ObjectId,
+    ref: 'album'
+  }]
+})
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = ArtistSchema
