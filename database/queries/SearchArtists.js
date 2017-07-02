c5f138388c6dd0770218fd9c0d272af872fafc7e
lgants <lgants@gmail.com>
2017-07-02 18:26:26 -0500

    added searh query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,8 +9,9 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
   // sortProperty is not an array; it uses ES6 interpolated properties (e.g. keys)
-  Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit)
@@ -25,3 +26,25 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
       }
     })
 };
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    }
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    }
+  }
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+}
