d7ad62d5a9ed950e2671bdfc7f61264b1060952c
lgants <lgants@gmail.com>
2017-07-02 18:55:18 -0500

    added set retired

--- database/queries/SetRetired.js
+++ database/queries/SetRetired.js
@@ -5,5 +5,8 @@ const Artist = require('../models/artist');
  * @param {array} _ids - An array of the _id's of of artists to update
  * @return {promise} A promise that resolves after the update
  */
+
+// { multi: true } necessary to update multiple records 
 module.exports = (_ids) => {
+  return Artist.update({ _id: { $in: _ids } }, { retired: true }, { multi: true })
 };
